64e954a480902fb3135a4db5be15022712fe8d13
Aayush Malhotra <37381075+AadumKhor@users.noreply.github.com>
2019-06-27 16:45:45 +0530

    Firebase.js added

---
+++ firebase.js
@@ -0,0 +1,32 @@
+var auth = require("firebaseauth");
+var firebase = new auth(process.env.AIzaSyBoLcgbWMf1hnTpvVYcVBcCCzZt5qiVERQ);
+
+const emailReg = firebase.registerWithEmail(email, password, (err, result) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(result);
+  }
+});
+
+const fbLog = firebase.loginWithFacebook(token, (err, result) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(result);
+  }
+});
+
+const googleLog = firebase.loginWithGoogle(token, (err, result) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(result);
+  }
+});
+
+module.exports = {
+  emailReg: emailReg,
+  fbLog: fbLog,
+  googleLog: googleLog
+};
