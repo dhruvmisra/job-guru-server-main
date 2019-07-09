f28704b8c6c42ace1d1211b8d7f0358ca8863390
shivank-dev <sharmashivank59@gmail.com>
2019-07-09 17:41:15 +0530

    package json updated

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const AppRouter = require("./Router/AppRouter");
 const bodyParser = require('body-parser');
-const app = express();
+const index = express();
 const port = 3000;
 const cors = require('cors');
 
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+index.use(cors());
+index.use(bodyParser.urlencoded({ extended: false }));
+index.use(bodyParser.json());
 
-new AppRouter(app).initRoutes();
+new AppRouter(index).initRoutes();
 
-app.listen(port, () => {
+index.listen(port, () => {
     console.log(`Server Listening on port`, port);
 });
